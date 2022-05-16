28eff9e576763e93d30884ba51bfc6948fbc018b
cesargpita <c.gonzapita@gmail.com>
2022-05-16 19:40:30 +0100

    1.11

--- part1/1.6-1.14/unicafe/src/App.js
+++ part1/1.6-1.14/unicafe/src/App.js
@@ -2,18 +2,30 @@ import { useState } from 'react'
 
 const Button = ({ text, clickHandler }) => <button onClick={clickHandler}>{text}</button>
 
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) =>
+  <tr>
+    <td>
+      {text}
+    </td>
+    <td>
+      {value}
+    </td>
+  </tr>
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   return <> {total ? <>
     <h2>statistics</h2>
-    <StatisticLine text="good" value={good} />
-    <StatisticLine text="neutral" value={neutral} />
-    <StatisticLine text="bad" value={bad} />
-    <StatisticLine text="all" value={total} />
-    <StatisticLine text="average" value={(good - bad) / total} />
-    <StatisticLine text="positive" value={good / total * 100 + '%'} />
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={(good - bad) / total} />
+        <StatisticLine text="positive" value={good / total * 100 + '%'} />
+      </tbody>
+    </table>
   </> : <p>No feedback given</p>}
   </>
 }
