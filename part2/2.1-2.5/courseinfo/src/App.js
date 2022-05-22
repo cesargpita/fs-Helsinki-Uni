9f4dcd834f718f0f8bc4358a25c78487c2011717
cesargpita <c.gonzapita@gmail.com>
2022-05-22 13:14:11 +0100

    2.1

---
+++ part2/2.1-2.5/courseinfo/src/App.js
@@ -0,0 +1,54 @@
+
+
+const Header = ({ name }) => {
+  return <>
+    <h1>{name}</h1>
+  </>
+}
+
+const Part = ({ name, exercises }) => <p>
+  {name} {exercises}
+</p>
+
+const Content = ({ parts }) => <>
+  {parts.map((part) => <Part name={part.name} exercises={part.exercises} key={part.id} />)}
+</>
+
+const Total = ({ parts }) => <>
+  <p>Number of exercises {parts.map(_ => _.exercises).reduce((a, b) => a + b, 0)}</p>
+</>
+
+const Course = ({ course }) =>
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App;
