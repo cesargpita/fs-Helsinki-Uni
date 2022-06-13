9441ff1caf5681f443d346004ec0e1985bfbb07a
cesargpita <c.gonzapita@gmail.com>
2022-06-13 20:21:21 +0100

    3.15

--- part3/phonebook-be/index.js
+++ part3/phonebook-be/index.js
@@ -15,11 +15,24 @@ const morganConfig = (tokens, req, res) => {
   ].join(' ')
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan(morganConfig))
 app.use(express.static('build'))
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 
 app.get('/', (request, response) => {
@@ -37,17 +50,23 @@ app.get('/info', (request, response) => {
   `)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(_ => _.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -77,7 +96,22 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
