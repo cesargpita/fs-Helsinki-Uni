d4c83a74f43c5402c2c6094de84c9a141185ad32
cesargpita <c.gonzapita@gmail.com>
2022-06-22 19:30:32 +0100

    4.4

--- part4/bloglistapplication/utils/list_helper.js
+++ part4/bloglistapplication/utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = blogs => {
+  return blogs.map(_ => _.likes).reduce((a, b) => a + b, 0)
+}
+
+const favouriteBlog = (blogs) => blogs.reduce((a, b) => a?.likes > b?.likes ? a : b, null)
+
+module.exports = { dummy, totalLikes, favouriteBlog }
