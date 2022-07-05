556e5caafb10a447a76b084ac4d8a46279da226a
cesargpita <c.gonzapita@gmail.com>
2022-07-05 20:31:03 +0100

    4.15

--- part4/bloglistapplication/app.js
+++ part4/bloglistapplication/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const blogRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
