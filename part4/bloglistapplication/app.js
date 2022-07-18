ae123e8259f329cf6ffaa9762761b2ca56b3e644
cesargpita <c.gonzapita@gmail.com>
2022-07-18 18:57:04 +0100

    4.20

--- part4/bloglistapplication/app.js
+++ part4/bloglistapplication/app.js
@@ -3,10 +3,12 @@ const cors = require('cors')
 const blogRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware');
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
--- part4/bloglistapplication/controllers/blog.js
+++ part4/bloglistapplication/controllers/blog.js
@@ -3,14 +3,6 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogRouter.get('', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
@@ -18,11 +10,10 @@ blogRouter.get('', async (request, response) => {
 
 blogRouter.post('', async (request, response) => {
   const { title, url, likes } = { ...request.body }
-  const token = getTokenFrom(request)
-  if (!token) {
+  if (!request.token) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
