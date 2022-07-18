19cad23413685c311fed2502d675a33a244f7640
cesargpita <c.gonzapita@gmail.com>
2022-07-18 18:46:24 +0100

    4.19

--- part4/bloglistapplication/controllers/blog.js
+++ part4/bloglistapplication/controllers/blog.js
@@ -1,5 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -7,11 +17,20 @@ blogRouter.get('', async (request, response) => {
 })
 
 blogRouter.post('', async (request, response) => {
-  const { title, author, url, likes } = { ...request.body }
+  const { title, url, likes } = { ...request.body }
+  const token = getTokenFrom(request)
+  if (!token) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   if (!title && !url) {
     response.status(400).send('title and url are missing!');
   } else {
-    const blog = new Blog({ title, author, url, likes: likes ? likes : 0 })
+    const blog = new Blog({ title, author: user.name, url, likes: likes ? likes : 0 })
     const result = await blog.save()
     response.status(201).json(result)
   }
