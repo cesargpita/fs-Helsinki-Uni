713d043fad0d96124541aa712b5efbdc65df2de4
cesargpita <c.gonzapita@gmail.com>
2022-08-20 13:44:20 +0100

    5.5

--- part5/bloglist-frontend/src/components/AddBlog.js
+++ part5/bloglist-frontend/src/components/AddBlog.js
@@ -5,6 +5,7 @@ const AddBlog = ({ setBlogs, blogs, showMessage }) => {
 
   const [title, setTitle] = useState('')
   const [url, setUrl] = useState('')
+  const [visibleForm, setVisibleForm] = useState(false)
 
   const addBlog = async (event) => {
     event.preventDefault()
@@ -13,27 +14,37 @@ const AddBlog = ({ setBlogs, blogs, showMessage }) => {
     setBlogs(blogs.concat(newBlog.data))
     setTitle('')
     setUrl('')
+    setVisibleForm(false)
     showMessage(`a new blog ${title} by ${window.localStorage.getItem('loggedBlogappUser').username} successfully created!`, 'success')
   }
 
+  const hideWhenVisible = { display: visibleForm ? 'none' : '' }
+  const showWhenVisible = { display: visibleForm ? '' : 'none' }
+
   return (
     <>
-      <h2>Create new</h2>
-      <form onSubmit={addBlog}>
-        <div>
-          title: <input
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-          />
-        </div>
-        <div>
-          url: <input
-            value={url}
-            onChange={(event) => setUrl(event.target.value)}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setVisibleForm(true)}>new note</button>
+      </div>
+      <div style={showWhenVisible}>
+        <h2>Create new</h2>
+        <form onSubmit={addBlog}>
+          <div>
+            title: <input
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+          </div>
+          <div>
+            url: <input
+              value={url}
+              onChange={(event) => setUrl(event.target.value)}
+            />
+          </div>
+          <button type="submit">create</button>
+          <button onClick={() => setVisibleForm(false)}>cancel</button>
+        </form>
+      </div>
     </>
   )
 }
