badca6eea4a11f771bf6dcd2b7c81022602ee362
cesargpita <c.gonzapita@gmail.com>
2022-08-27 19:13:36 +0100

    5.6&5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,28 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  console.log(blog)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visibleBlog, setVisibleBlog] = useState(false)
+  return (
+    <div style={blogStyle}>
+      <div>{blog.title} <button onClick={() => setVisibleBlog(!visibleBlog)}>{visibleBlog ? 'hide' : 'view'}</button></div>
+      {visibleBlog && <div>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}<button>like</button></div>
+        <div>{blog.author}</div>
+      </div>}
+    </div>
+  )
+}
 
 export default Blog
