0db61509720e53be1764763e04904dead1547416
cesargpita <c.gonzapita@gmail.com>
2022-12-28 20:01:58 +0000

    6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -4,23 +4,27 @@ import { createStore } from 'redux'
 import reducer from './reducer'
 
 const store = createStore(reducer)
+store.subscribe(() => {
+  const storeNow = store.getState()
+  console.log(storeNow)
+})
 
 const App = () => {
-  const good = () => {
+  const dispatch = (status) => {
     store.dispatch({
-      type: 'GOOD'
+      type: status
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => dispatch('GOOD')}>good</button>
+      <button onClick={() => dispatch('OK')}>ok</button>
+      <button onClick={() => dispatch('BAD')}>bad</button>
+      <button onClick={() => dispatch('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
